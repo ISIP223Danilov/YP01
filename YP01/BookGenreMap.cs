3f301ee72af650b7fb4ed09923ceef4ed87cabe4
Valery <vsler@YCHEBA>
2026-05-27 02:23:57 +0300

    Все страницы

---
+++ YP01/BookGenreMap.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YP01
+{
+    [Table("BookGenreMap")]
+    public class BookGenreMap
+    {
+        [Key]
+        [Column(Order = 0)]
+        public int BookId { get; set; }
+
+        [Key]
+        [Column(Order = 1)]
+        public int GenreId { get; set; }
+
+        [ForeignKey("BookId")]
+        public virtual LibraryBooks LibraryBooks { get; set; }
+
+        [ForeignKey("GenreId")]
+        public virtual GenreCatalog GenreCatalog { get; set; }
+    }
+}
